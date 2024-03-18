bae4168aedb191c584bb5d3e19088dd80e1dfa49
hortfrancis <ajhf@hotmail.co.uk>
2024-03-18 20:57:38 +0000

    Transcribed text displayed on the page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,34 +2,20 @@ import { useState, useEffect } from 'react';
 
 export default function App() {
     const [recording, setRecording] = useState(false);
+    const [processing, setProcessing] = useState(false);
     const [mediaRecorder, setMediaRecorder] = useState(null);
     const [audioChunks, setAudioChunks] = useState([]);
+    const [transcriptions, setTranscriptions] = useState([]);
 
     useEffect(() => {
-        (async () => {
-
-            if (audioChunks.length > 0 && !recording) {
+        if (audioChunks.length > 0 && !recording) {
+            (async () => {
                 console.log('audioChunks.length:', audioChunks.length)
-                const audioBlob = new Blob(audioChunks, { type: 'audio/flac' });
-                const formData = new FormData();
-                formData.append('audio', audioBlob);
-
-                try {
-                    console.log('Sending audio to server...');
-                    const response = await fetch('http://localhost:3000/speech-to-text', {
-                        method: 'POST',
-                        body: formData,
-                    });
-                    const data = await response.json();
-                    console.log(data);
-                } catch (error) {
-                    console.error('Error sending audio to server:', error);
-                }
-
+                sendAudioToServer();
                 // Clear the audioChunks after sending the data to the server
                 setAudioChunks([]);
-            }
-        })();
+            })();
+        }
     }, [audioChunks]);
 
     async function startRecording() {
@@ -50,6 +36,7 @@ export default function App() {
     }
 
     function stopRecording() {
+        setProcessing(true);
         // Wait 1 second for the MediaRecorder to finish recording
         setTimeout(() => {
 
@@ -58,25 +45,26 @@ export default function App() {
             setMediaRecorder(null);
             console.log('Recording stopped');
             setRecording(false);
+            setProcessing(false);
         }, 1000);
     }
 
     async function sendAudioToServer() {
-        if (audioChunks.length > 0) {
-            const audioBlob = new Blob(audioChunks, { type: 'audio/flac' });
-            const formData = new FormData();
-            formData.append('audio', audioBlob);
+        const audioBlob = new Blob(audioChunks, { type: 'audio/flac' });
+        const formData = new FormData();
+        formData.append('audio', audioBlob);
 
-            try {
-                const response = await fetch('http://localhost:3000/speech-to-text', {
-                    method: 'POST',
-                    body: formData,
-                });
-                const data = await response.json();
-                console.log(data);
-            } catch (error) {
-                console.error('Error sending audio to server:', error);
-            }
+        try {
+            console.log('Sending audio to server...');
+            const response = await fetch('http://localhost:3000/speech-to-text', {
+                method: 'POST',
+                body: formData,
+            });
+            const data = await response.json();
+            console.log(data);
+            setTranscriptions(prev => [...prev, ...data.transcriptions]);
+        } catch (error) {
+            console.error('Error sending audio to server:', error);
         }
     }
 
@@ -86,10 +74,12 @@ export default function App() {
                 className={`px-4 py-2 text-2xl font-semibold text-white transition-colors duration-200 rounded-lg shadow 
                     ${recording ? 'bg-red-600 hover:bg-red-700' : 'bg-blue-600 hover:bg-blue-700'} `}
                 onClick={!recording ? startRecording : stopRecording}
-            >{!recording ? 'Speak' : 'Send'}</button>
-            {audioChunks.map((chunk, index) => (
-                <audio key={index} controls src={URL.createObjectURL(chunk)} className="block w-full mt-4" />
-            ))}
+            >{!recording ? 'Speak' : 'Send'} {processing ? '⏳' : ''}</button>
+            <div className="mt-4">
+                {transcriptions.map((transcription, index) => (
+                    <p key={index}>{transcription}</p>
+                ))}
+            </div>
         </div>
     )
 }
